1b68fa97c96af3f5553f7e356c0e9d20b1ae6527
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-28 15:23:11 -0300

    Test Behavior when Clicking Button

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import App from './App'
 
-test('button has correct initial color', () => {
+test('button has correct initial color, and updates when clicked', () => {
   render(<App />)
 
   // find an element with a role of button and text of 'Change to blue'
@@ -9,8 +9,13 @@ test('button has correct initial color', () => {
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ 'background-color': 'red' })
-})
 
-test('button turns blue when clicked', () => {
-  
+  // click button
+  fireEvent.click(colorButton)
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ 'background-color': 'blue' })
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent('Change to red')
 })
