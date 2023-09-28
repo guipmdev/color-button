678fa2c9440b92e705b9b656ba5b99fc0b959f09
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-28 16:14:35 -0300

    Click Button to Change Color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red')
+
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
+
+  function handleChangeButtonColor() {
+    setButtonColor(newButtonColor)
+  }
+
   return (
     <div>
-      <button style={{backgroundColor: 'red'}}>Change to blue</button>
+      <button
+        style={{backgroundColor: buttonColor}}
+        onClick={handleChangeButtonColor}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
