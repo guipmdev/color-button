aa2a385dcdf51a6f7cf32f52bc5c78eea9c759db
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-28 14:58:38 -0300

    Start Color Button App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('button has correct initial color', () => {
+  render(<App />)
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ 'background-color': 'red' })
+})
+
+test('button turns blue when clicked', () => {
+  
+})
